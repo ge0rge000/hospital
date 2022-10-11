b6cf739e97d265d0287c3c4797357afaeb31a3d3
george samy <56982844+ge0rge000@users.noreply.github.com>
2022-10-11 21:15:22 +0200

    go

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,17 +1,25 @@
 import 'package:get/get.dart';
 
+import '../view/screens/auth/login_screen.dart';
+import '../view/screens/auth/signup_Screen.dart';
 import '../view/screens/welcome_screen.dart';
 
 class AppRoutes{
 
   static const welcome =Routes.welcomeScreen;
+  static const loginscreen =Routes.loginscreen;
+  static const signupscreen =Routes.signupscreen;
   static final routes=[
-    GetPage(name: '/welcomescreen', page:()=> const WelcomeScreen()),
+    GetPage(name: Routes.welcomeScreen, page:()=> const WelcomeScreen()),
+    GetPage(name: Routes.loginscreen, page:()=> const LoginScreen()),
+    GetPage(name: Routes.signupscreen, page:()=> const SignUpScreen()),
 
   ];
 }
 
 class Routes{
   static const  welcomeScreen='/welcomeScreen';
+  static const loginscreen='/loginscreen';
+  static const signupscreen='/signupscreen';
 
 }
---
+++ lib/view/widgets/text_utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TextUtils extends StatelessWidget {
+   final String text;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color color;
+  final TextDecoration? decoration;
+  TextUtils({required this.fontSize,required this.text,required this.fontWeight,Key? key, required this.color,  this.decoration ,   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Text( text,style:
+    TextStyle(
+      decoration: decoration,
+    color: color,
+        fontSize:fontSize,
+        fontWeight: FontWeight.bold
+    ));
+  }
+}
